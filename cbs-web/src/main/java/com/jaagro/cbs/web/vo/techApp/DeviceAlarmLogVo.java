e749a80fde0b4dde700a7a85db3814228d1e967b
wanghq <gavinwang0224@aliyun.com>
2019-04-08 15:22:35 +0800

    技术App-待办报警列表

---
+++ cbs-web/src/main/java/com/jaagro/cbs/web/vo/techApp/DeviceAlarmLogVo.java
@@ -0,0 +1,45 @@
+package com.jaagro.cbs.web.vo.techApp;
+
+
+import com.jaagro.cbs.api.dto.techSiteApp.DeviceAlarmLogDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author gavin
+ * @Date 20190408
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class DeviceAlarmLogVo extends DeviceAlarmLogDto implements Serializable {
+
+    /**
+     * 设备的最近一次报警值
+     */
+    private BigDecimal latestValue;
+
+
+    /**
+     * 设备的最近一次报警值时间
+     */
+    private Date latestAlarmDate;
+
+    /**
+     * 客户联系人名称
+     */
+    private String customerContactName;
+
+    /**
+     * 联系人电话
+     */
+    private String customerContactPhone;
+
+}
