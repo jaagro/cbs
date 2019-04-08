b0636cf8731612d214deb2444ccfa5fc2492bb29
baiyiran <1169172027@qq.com>
2019-04-08 10:44:00 +0800

    技术app端  建包

--- cbs-web/src/main/java/com/jaagro/cbs/web/controller/technicianapp/TechnicianAppController.java
+++ cbs-web/src/main/java/com/jaagro/cbs/web/controller/technicianapp/TechnicianAppController.java
@@ -6,13 +6,13 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 技术app端管理
+ * 技术管理app端
  *
  * @author baiyiran
  * @date 2019-04-08
  */
 @RestController
-@Api(description = "技术app端管理", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(description = "技术管理app端", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class TechnicianAppController {
 }
