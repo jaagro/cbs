c4defbac94f75e402c999885bfe9846a43fd7667
wanghq <gavinwang0224@aliyun.com>
2019-04-09 17:21:32 +0800

    抛异常修改

--- cbs-web/src/main/java/com/jaagro/cbs/web/controller/technicianapp/TechnicianAppController.java
+++ cbs-web/src/main/java/com/jaagro/cbs/web/controller/technicianapp/TechnicianAppController.java
@@ -1,7 +1,6 @@
 package com.jaagro.cbs.web.controller.technicianapp;
 
 import com.github.pagehelper.PageInfo;
-import com.github.pagehelper.PageInfo;
 import com.jaagro.cbs.api.dto.base.CustomerContactsReturnDto;
 import com.jaagro.cbs.api.dto.farmer.BreedingBatchParamDto;
 import com.jaagro.cbs.api.dto.plan.ReturnBreedingPlanDto;
@@ -14,12 +13,12 @@ import com.jaagro.cbs.api.enums.PurchaseOrderStatusEnum;
 import com.jaagro.cbs.api.model.Product;
 import com.jaagro.cbs.api.model.PurchaseOrderItems;
 import com.jaagro.cbs.api.service.BreedingPlanService;
-import com.jaagro.cbs.web.vo.technicianapp.UnConfirmChickenPlanVo;
 import com.jaagro.cbs.api.service.BreedingPurchaseOrderService;
 import com.jaagro.cbs.api.service.ProductService;
 import com.jaagro.cbs.biz.service.CustomerClientService;
 import com.jaagro.cbs.web.vo.technicianapp.AppPurchaseOrderItemsVo;
 import com.jaagro.cbs.web.vo.technicianapp.AppPurchaseOrderVo;
+import com.jaagro.cbs.web.vo.technicianapp.UnConfirmChickenPlanVo;
 import com.jaagro.utils.BaseResponse;
 import com.jaagro.utils.ResponseStatusCode;
 import io.swagger.annotations.Api;
@@ -29,16 +28,15 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.util.CollectionUtils;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 技术管理app端
